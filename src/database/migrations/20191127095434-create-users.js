15283d33d3748d24f55ad3e33f3a205783d877fe
Dino Tudor <dinotudor@gmail.com>
2019-11-27 11:01:42 +0100

    create migration test user creation

---
+++ src/database/migrations/20191127095434-create-users.js
@@ -0,0 +1,38 @@
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        tyoe: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        tyoe: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('users');
+  },
+};
