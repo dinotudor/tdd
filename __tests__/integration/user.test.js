43a7f49e38b9b898399ee7916a460897bcd78c77
Dino Tudor <dinotudor@gmail.com>
2019-11-30 17:10:03 +0100

    add test to check password encryption

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,11 +1,25 @@
 import request from 'supertest';
+import bcrypt from 'bcryptjs';
 import app from '../../src/app';
 
+import User from '../../src/app/models/User';
 import truncate from '../util/truncate';
 
 describe('User', () => {
   beforeEach(async () => {
     await truncate();
+  });
+
+  it('should encrypt user password when creating a profile', async () => {
+    const user = await User.create({
+      name: 'Dino Tudor',
+      email: 'dino@dino.com',
+      password: '123456',
+    });
+
+    const compareHash = await bcrypt.compare('123456', user.password_hash);
+
+    expect(compareHash).toBe(true);
   });
 
   it('should be able to register', async () => {
