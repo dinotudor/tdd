3a1d368b2742991efad5b80176d72dfdec4cc975
Dino Tudor <dinotudor@gmail.com>
2019-11-30 19:37:23 +0100

    implement factory girl for user

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -21,32 +21,25 @@ describe('User', () => {
   });
 
   it('should be able to register', async () => {
+    const user = await factory.attrs('User');
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Dino Tudor',
-        email: 'dino@dino.com',
-        password: '123456',
-      });
+      .send(user);
 
     expect(response.body).toHaveProperty('id');
   });
 
   it('should not be able to register with duplicated email', async () => {
+    const user = await factory.attrs('User');
+
     await request(app)
       .post('/users')
-      .send({
-        name: 'Dino Tudor',
-        email: 'dino@dino.com',
-        password: '123456',
-      });
+      .send(user);
+
     const response = await request(app)
       .post('/users')
-      .send({
-        name: 'Dino Tudor',
-        email: 'dino@dino.com',
-        password: '123456',
-      });
+      .send(user);
     expect(response.status).toBe(400);
   });
 });
